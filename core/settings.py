248df2faac512936bde6acf27b925bc84d202b04
Ishmael <abuishmaelyusif204@gmail.com>
2024-07-14 11:32:32 -1200

    commit

--- core/settings.py
+++ core/settings.py
@@ -74,14 +74,21 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'jiji-frontend',
+#         'USER': 'postgres',
+#         'PASSWORD': 'pass123',
+#         'HOST': 'localhost',
+#         'PORT': 5432,
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'jiji_frontend_yusif',
-        'USER': 'postgres',
-        'PASSWORD': 'pass123',
-        'HOST': 'localhost',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
