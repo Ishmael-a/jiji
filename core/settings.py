a051c3bce37838e3bf647f909a5ceb787cde8fde
Ishmael <abuishmaelyusif204@gmail.com>
2024-07-16 06:19:16 -1200

    changed host

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-eok5jn3j$!kp&%2bwa(@ivl(ql3u)-e%$39n55hb!kiekep=81
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['Ishmaelllll.pythonanywhere.com']
+ALLOWED_HOSTS = ['jiji-angt.onrender.com']
 
 
 # Application definition
